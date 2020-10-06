f3f6f797e630b5e59915564e5e52e0b3a3039a2f
Matt Miller <matt.miller@bronto.com>
2020-10-06 09:31:36 -0400

    Generalize fastSearch to have a variable number of partitions

--- src/main/java/com/mattmill/PartitionFinder.java
+++ src/main/java/com/mattmill/PartitionFinder.java
@@ -6,7 +6,10 @@ import java.util.stream.IntStream;
 
 public class PartitionFinder {
 
-    public static long fastSearch(String theString) {
+    public static long fastSearch(String theString, int numPartitions) {
+        if (numPartitions < 2 || theString.isEmpty()) {
+            return 0;
+        }
         // List which describes the index of a given 'a'
         // If we want the 3rd 'a', then aIndexes.get(3) will give us it.
         // aIndexes.size() also gives us the total number of a's in the string
@@ -17,22 +20,22 @@ public class PartitionFinder {
 
         // If there are the wrong number of a's to partition them all evenly, then the number of
         // unique combinations is always 0
-        if(aIndexes.size() % 3 != 0 || theString.isEmpty()) {
+        if (aIndexes.size() % numPartitions != 0) {
             return 0;
         } else if (aIndexes.isEmpty()) {
             // there are no a's so every combination has an equal number of a's.  the problem now depends on
             // how many unique ways we can split the string up.
             return 0;
         } else {
-            int numPerPartition = (aIndexes.size() / 3);
+            int numPerPartition = (aIndexes.size() / numPartitions);
             long uniqueWaysToSplit = 1; //accumulator
-            for(int currentBucket = 1; currentBucket<3; currentBucket++) {
-                int currentIdx = (numPerPartition * currentBucket)-1;
+            for (int currentBucket = 1; currentBucket < numPartitions; currentBucket++) {
+                int currentIdx = (numPerPartition * currentBucket) - 1;
                 // The position in the string of the last A in the Nth partition
                 Integer firstA = aIndexes.get(currentIdx);
                 // The position in the string of the first A in the N+1th partition
-                Integer secondA = aIndexes.get(currentIdx+1);
-                uniqueWaysToSplit = uniqueWaysToSplit * (secondA-firstA);
+                Integer secondA = aIndexes.get(currentIdx + 1);
+                uniqueWaysToSplit = uniqueWaysToSplit * (secondA - firstA);
             }
             return uniqueWaysToSplit;
         }
--- src/test/java/com/mattmill/PartitionFinderTest.java
+++ src/test/java/com/mattmill/PartitionFinderTest.java
@@ -10,46 +10,68 @@ public class PartitionFinderTest {
 
     @Test
     public void testSplit_empty() {
-        testInput("", 0);
+        testInput("", 3, 0);
     }
 
     @Test
     public void testSplit_onlyAs() {
-        testInput("aaa", 1);
-        testInput("aaaa", 0);
-        testInput("aaaaa", 0);
-        testInput("aaaaaa", 1);
+        testInput("aaa", 3, 1);
+        testInput("aaaa", 3, 0);
+        testInput("aaaaa", 3, 0);
+        testInput("aaaaaa", 3, 1);
+
+        testInput("aaa", 4, 0);
+        testInput("aaaa", 4, 1);
+        testInput("aaaaa", 4, 0);
+        testInput("aaaaaaaa", 4, 1);
     }
 
     @Test
     public void testSplit_onlyBs() {
-        testInput("b", 0);
-        testInput("bb", 0);
-        testInput("bbb", 0);
+        testInput("b", 3, 0);
+        testInput("bb", 3, 0);
+        testInput("bbb", 3, 0);
+
+        testInput("b", 4, 0);
+        testInput("bb", 4, 0);
+        testInput("bbb", 4, 0);
     }
 
     @Test
     public void testSplit_ababab() {
-        testInput("ababab", 4);
-        testInput("ababbab", 6);
-        testInput("abbabab", 6);
-        testInput("abbababb", 6);
-        testInput("abbabbab", 9);
-        testInput("abbabbabb", 9);
+        testInput("ababab", 3, 4);
+        testInput("ababbab", 3, 6);
+        testInput("abbabab", 3, 6);
+        testInput("abbababb", 3, 6);
+        testInput("abbabbab", 3, 9);
+        testInput("abbabbabb", 3, 9);
+    }
+
+    @Test
+    public void testSplit_ababab_4partitions() {
+        testInput("abababab", 4, 8);
+        testInput("ababbabab", 4, 12);
+        testInput("abbababab", 4, 12);
+        testInput("abbababbab", 4, 18);
+        testInput("abbabbabab", 4, 18);
+        testInput("abbabbabbab", 4, 27);
     }
 
     @Test
     public void testSplit_manyAs() {
-        testInput("aaaaaaaaa", 1);
-        testInput("aaabbabbabbabaaa", 6);
+        testInput("aaaaaaaaa", 3, 1);
+        testInput("aaabbabbabbabaaa", 3, 6);
+        testInput("aaaaaaaa", 4, 1);
+        testInput("aaabbabbabbabaa", 4, 6);
     }
 
     @Test
     public void testSplit_notEnoughAs() {
-        testInput("abbabb", 0);
+        testInput("abbabb", 3, 0);
+        testInput("abbabb", 4, 0);
     }
 
-    private void testInput(String input, int expectedNumberOfPartitions){
-        Assert.assertEquals(expectedNumberOfPartitions, PartitionFinder.fastSearch(input));
+    private void testInput(String input, int numPartitions, int expectedResult) {
+        Assert.assertEquals(expectedResult, PartitionFinder.fastSearch(input, numPartitions));
     }
 }
